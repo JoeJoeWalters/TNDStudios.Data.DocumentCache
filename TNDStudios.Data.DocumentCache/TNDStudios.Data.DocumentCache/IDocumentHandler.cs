30c495ce4c8ce01de91a2ca309f20e3e180bf103
TNDStudios <info@thenakeddeveloper.com>
2019-03-10 01:16:13 +0000

    Initial check in with interface, document wrapper and an Azure Cosmos DB implementation of the document caching

---
+++ TNDStudios.Data.DocumentCache/TNDStudios.Data.DocumentCache/IDocumentHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TNDStudios.Data.DocumentCache
+{
+    public interface IDocumentHandler<T>
+    {
+        /// <summary>
+        /// Public view of the currently connection
+        /// </summary>
+        string ConnectionString { get; }
+
+        /// <summary>
+        /// Database Name for the cache
+        /// </summary>
+        string DatabaseName { get; }
+
+        /// <summary>
+        /// Collection (table / collection / grouping) for this cache
+        /// </summary>
+        string DataCollection { get; }
+
+        /// <summary>
+        /// Send the object to the cache
+        /// </summary>
+        /// <typeparam name="T">The data type that is being sent to storage</typeparam>
+        /// <param name="id">The value of the new document to be put in storage (as the json has to be case sensitive)</param>
+        /// <param name="data">The data to be wrapped up in the cache document</param>
+        /// <returns>Success Result</returns>
+        Boolean SendToCache(string id, T data);
+    }
+}
