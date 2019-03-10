6c22e21a1d9a0dd17a5fe09b8df6121562a3516d
TNDStudios <info@thenakeddeveloper.com>
2019-03-10 15:17:38 +0000

    Memory Cache Implementation

--- Package/Implementations/Memory/MemoryDocumentHandler.cs
+++ Package/Implementations/Memory/MemoryDocumentHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TNDStudios.Data.DocumentCache.Implementations.Memory
@@ -15,7 +16,7 @@ namespace TNDStudios.Data.DocumentCache.Implementations.Memory
         /// <summary>
         /// Where documents are stored in memory
         /// </summary>
-        private List<T> documentCache;
+        private Dictionary<String, DocumentWrapper<T>> documentCache;
 
         /// <summary>
         /// Public view of the currently connection
@@ -33,6 +34,20 @@ namespace TNDStudios.Data.DocumentCache.Implementations.Memory
         public String DataCollection { get; internal set; } = String.Empty;
 
         /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public MemoryDocumentHandler(ILogger logger, String connectionString, String databaseName, String dataCollection)
+        {
+            this.ConnectionString = connectionString;
+            this.DatabaseName = databaseName;
+            this.DataCollection = dataCollection;
+
+            this.logger = logger;
+
+            this.documentCache = new Dictionary<String, DocumentWrapper<T>>();
+        }
+
+        /// <summary>
         /// Send the object to the cache
         /// </summary>
         /// <typeparam name="T">The data type that is being sent to storage</typeparam>
@@ -41,7 +56,20 @@ namespace TNDStudios.Data.DocumentCache.Implementations.Memory
         /// <returns>Success Result</returns>
         public bool Save(string id, T data)
         {
-            throw new NotImplementedException();
+            // Add the document to the dictionary of documents
+            try
+            {
+                documentCache[id] = new DocumentWrapper<T>()
+                {
+                    Id = id,
+                    Data = data,
+                    CreatedDateTime = DateTime.UtcNow
+                };
+            }
+            catch { }
+
+            // Did it get added?
+            return documentCache.ContainsKey(id);
         }
 
         /// <summary>
@@ -49,25 +77,94 @@ namespace TNDStudios.Data.DocumentCache.Implementations.Memory
         /// </summary>
         /// <param name="id">The id of the document</param>
         /// <returns>The document from the cache</returns>
-        public T Get(string id) { throw new NotImplementedException(); }
+        public T Get(string id)
+        {
+            if (documentCache.ContainsKey(id))
+                return documentCache[id].Data;
+            else
+                return default(T);
+        }
 
         /// <summary>
         /// Get all items that are marked a having not been processed yet
         /// </summary>
         /// <returns>A list of unprocessed items</returns>
-        public List<T> GetToProcess(Int32 maxRecords) { throw new NotImplementedException(); }
+        public List<T> GetToProcess(Int32 maxRecords) =>
+            documentCache
+                .Where(keyValuePair => !keyValuePair.Value.Processed)
+                .Select(keyValuePair => keyValuePair.Value.Data)
+                .ToList<T>();
 
         /// <summary>
         /// Mark a set of documents as processed
         /// </summary>
         /// <param name="documentsToMark">A list of document id's to mark as processed</param>
         /// <returns>The id's of the documents that did get marked</returns>
-        public List<String> MarkAsProcessed(List<String> documentsToMark) { throw new NotImplementedException(); }
+        public List<String> MarkAsProcessed(List<String> documentsToMark)
+        {
+            try
+            {
+                // Attempt to mark the items
+                documentCache
+                .Where(document => documentsToMark.Contains(document.Key))
+                .ToList()
+                .ForEach(document =>
+                    {
+                        document.Value.Processed = true;
+                        document.Value.ProcessedDateTime = DateTime.UtcNow;
+                    });
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failure to purge processed items from document cache");
+            }
+
+            // Return a list of the items that successfully marked as processed
+            try
+            {
+                return documentCache
+                    .Where(document =>
+                        (documentsToMark.Contains(document.Key) && document.Value.Processed))
+                    .Select(document => document.Key)
+                    .ToList<String>();
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex, "Failure to return keys of items marked as processed from document cache");
+            }
+
+            return new List<string>(); // Catastrophic failure where couldn't stamp and couldn't return
+        }
 
         /// <summary>
         /// Purge (Delete) all items marked as processed
         /// </summary>
         /// <returns>If the purge was successful</returns>
-        public Boolean Purge() { throw new NotImplementedException(); }
+        public Boolean Purge()
+        {
+            try
+            {
+                // Because of parallelism
+                List<String> keysToPurge = documentCache
+                    .Where(document => document.Value.Processed)
+                    .Select(document => document.Key)
+                    .ToList<String>();
+
+                // Remove the keys
+                keysToPurge.ForEach(key => documentCache.Remove(key));
+
+                // Check the keys are removed (not that all items are purged)
+                // as there may have been more added during the process
+                return documentCache
+                    .Select(document => keysToPurge.Contains(document.Key))
+                    .Count() == 0;
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex, "Failure to purge processed items from document cache");
+            }
+
+            return false;
+        }
     }
 }
